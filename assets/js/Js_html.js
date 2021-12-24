3cdd3925c96c8e2cddbca46bc159cbfd25e82762
enri-ca <72966913+enri-ca@users.noreply.github.com>
2021-12-24 11:51:47 +0100

    Update Js_html.js

--- assets/js/Js_html.js
+++ assets/js/Js_html.js
@@ -39,34 +39,4 @@ function change_issue(sw) {
    $("#artM").load(art1);
    $("#artL1").load(art2);
    $("#artL2").load(art3)
-    }
-
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
 }
-
-//function browse_issue(sw) tipo quella prima ma con scorrimento lista, forse col for loop//

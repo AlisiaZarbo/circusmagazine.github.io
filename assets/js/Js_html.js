0046e414c38ceb89ca56b71cead20d3611835029
enri-ca <72966913+enri-ca@users.noreply.github.com>
2021-12-24 01:03:07 +0100

    Update Js_html.js

--- assets/js/Js_html.js
+++ assets/js/Js_html.js
@@ -19,6 +19,14 @@ function change_css2(style) {
   document.getElementById('css').href = n_css;
 }
 
+$(document).ready(function(){
+	$("#A3").click(function(){
+    $(function(){
+      $("#artM").load("1_1.html"); 
+    });
+});
+ })
+
 function change_issue(sw) {
   var art1, art2, art3;
 	if (sw == 1) {
@@ -36,9 +44,9 @@ function change_issue(sw) {
 	art2 = '3_2.html';
 	art3 = '3_3.html'
   }
-	$("#artM").load("art1");
-	$("#artL1").load("art2");
-	$("#artL2").load("art3");
+	document.getElementById('artM').setAttribute('w3-include-html', art1);
+	document.getElementById('artL1').setAttribute('w3-include-html', art2);
+	document.getElementById('artL2').setAttribute('w3-include-html', art3);
 	}
 
 function includeHTML() {

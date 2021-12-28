0b0248b981eb4b1f2f9168484b632c0f1867b056
enri-ca <72966913+enri-ca@users.noreply.github.com>
2021-12-28 16:07:25 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -35,19 +35,48 @@ function change_issue(issue) {
 		art3 = "1_3.html";
 	  	}
 	if (issue == 2) {
-		art1 = '2_1.html';
-		art2 = '2_2.html';
-		art3 = '2_3.html';
+		art1 = "2_1.html";
+		art2 = "2_2.html";
+		art3 = "2_3.html";
 		}
 	if (issue == 3) {
-		art1 = "'3_1.html'";
-		art2 = '3_2.html';
-		art3 = '3_3.html';
+		art1 = "3_1.html";
+		art2 = "3_2.html";
+		art3 = "3_3.html";
 		}
 	$("#artM").load(art1);
 	$("#artL1").load(art2);
 	$("#artL2").load(art3);
 }
+
+counter=0
+function slide_articles(){
+counter=counter+1;
+if (counter==1) {
+      $("#artM").load("1_2.html");
+	$("#artL1").load("1_3.html");
+	$("#artL2").load("1_1.html");
+}
+if (counter==2) {
+    $("#artM").load("1_3.html");
+	$("#artL1").load("1_1.html");
+	$("#artL2").load("1_2.html");
+}	
+if (counter==3) {
+   	$("#artM").load("1_1.html");
+	$("#artL1").load("1_2.html");
+	$("#artL2").load("1_3.html");
+counter = 0;
+}
+}
+
+function entities(){
+	$(".entity").hide(); 
+	$(".entity").show(); 
+}
+
+//function browse_issue(sw) tipo quella prima ma con scorrimento lista, forse col for loop
+
 
 //$(document).ready(function(){
 //	$("#I1").click(function(){
@@ -78,31 +107,3 @@ function change_issue(issue) {
 //    });
 //});
 // })
-
-counter=0
-function slide_articles(){
-counter=counter+1;
-if (counter==1) {
-      $("#artM").load("1_2.html");
-	$("#artL1").load("1_3.html");
-	$("#artL2").load("1_1.html");
-}
-if (counter==2) {
-    $("#artM").load("1_3.html");
-	$("#artL1").load("1_1.html");
-	$("#artL2").load("1_2.html");
-}	
-if (counter==3) {
-   	$("#artM").load("1_1.html");
-	$("#artL1").load("1_2.html");
-	$("#artL2").load("1_3.html");
-counter = 0;
-}
-}
-
-function entities(){
-	$(".entity").hide(); 
-	$(".entity").show(); 
-}
-
-//function browse_issue(sw) tipo quella prima ma con scorrimento lista, forse col for loop

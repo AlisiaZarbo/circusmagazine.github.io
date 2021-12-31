28eaa0ebaab33600b6c589a7b59af522b8bf3a3a
enri-ca <72966913+enri-ca@users.noreply.github.com>
2021-12-31 13:58:17 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -81,10 +81,10 @@ function slide_articles(){
 
 //function to high_href() [working off-line not online]//
 //function high_href(){//
-//$(document).ready(function(){//
+//$(window).load(function(){//
 //window.onload = function(){//
 //function high_href(){//
-$(window).load(function(){
+$(document).ready(function()
 	$(".bib").click(function(){
 		var target = $(this).attr("href");
 		var source_id = $(this).attr("id");

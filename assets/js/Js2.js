7ac5ec9a6857a07a719b407da68de5ee5f748242
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-02 09:28:58 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -115,3 +115,20 @@ function slide_articles(){
 //    });
 //});
 // })
+
+//function to high_href() [working off-line with $(document).ready(function(){ not online]//
+//function high_href(){//
+//$(window).load(function(){//
+//window.onload = function(){//
+//function high_href(){//
+//$(document).ready(function(){//
+$(".bib_note").click(function(){
+		var target = $(this).attr("href");
+		var source_id = $(this).attr("id");
+		var source = document.getElementById(source_id);
+		$(target).addClass('highlighted');
+		$(target).click(function(){
+			$(target).removeClass('highlighted');
+			source.scrollIntoView();
+		});
+	})

3f95cf02184cce9dbaec9bd0a1e8a74518fab857
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-04 16:51:14 +0100

    Update Js.js

--- assets/js/Js.js
+++ assets/js/Js.js
@@ -1,9 +1,12 @@
-//Js principale - riportate prove solo dopo funzionamento, collegato a MAINFRAME//
+//copia Js per prove modifica su mainframe2//
+
+//function change_css//
 function change_css(style) {
 	document.getElementById('css').href = style
 	//setAttribute('href', style);
 }
 
+//function change_css 2nd version (I prefere the first one)//
 function change_css2(style) {
 	var n_css;
 	if (style == 0) {
@@ -25,12 +28,14 @@ art1 = "1_1.html";
 art2 = "1_2.html";
 art3 = "1_3.html";
 
+//function load default article in the frame{//
 $(document).ready(function(){
 	$("#artM").load(art1);
 	$("#artL1").load(art2);
 	$("#artL2").load(art3);
-  })
+	})
 
+//function change_issue//
 function change_issue(issue) {
 	if (issue == 1) {
 		art1 = "1_1.html";
@@ -52,6 +57,7 @@ function change_issue(issue) {
 	$("#artL2").load(art3);
 }
 
+//function slide_articles() in an issue//
 counter=0
 function slide_articles(){
 	counter=counter+1;
@@ -73,12 +79,31 @@ function slide_articles(){
 	}
 }
 
-//fare funzione per illuminare entities//
-function entities(){
-	$(".entity").hide(); 
-	$(".entity").show(); 
-}
+function MDV(selection){
+	let text = "";
+	if (selection == 1){
+		classname="entity person";
+		id = "PE"};
+	if (selection == 2){
+		classname="entity place";
+		id = "PL"};
+	if (selection == 3){
+		classname="entity keyword";
+		id = "KW"};
+	var arr = document.getElementsByClassName(classname);
+	for (let i = 0; i < arr.length; i++) {
+		text += '<a class="dropdown-item" href="https://www.unibo.it" onclick="openpopup()">' + arr[i].getAttribute("data-label") + "</a>"
+	}
+	document.getElementById(id).innerHTML = text;
+ }
 
+//function to hide/show entities [not working]//
+//function entities(){//
+//	$(".entity").hide(); 
+//	$(".entity").show(); 
+//}
+
+//previous attempts to load issues//
 //$(document).ready(function(){
 //	$("#I1").click(function(){
 //    $(function(){
@@ -108,3 +133,20 @@ function entities(){
 //    });
 //});
 // })
+
+//function to high_href() [working off-line with $(document).ready(function(){ not online]//
+//function high_href(){//
+//$(window).load(function(){//
+//window.onload = function(){//
+//function high_href(){//
+//$(document).ready(function(){//
+$(".bib_note").click(function(){
+		var target = $(this).attr("href");
+		var source_id = $(this).attr("id");
+		var source = document.getElementById(source_id);
+		$(target).addClass('highlighted');
+		$(target).click(function(){
+			$(target).removeClass('highlighted');
+			source.scrollIntoView();
+		});
+	})

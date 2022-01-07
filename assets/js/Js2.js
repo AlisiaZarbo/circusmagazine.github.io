25502c890bad8c8c8e8e8b754860b132c80faadb
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-07 08:47:45 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -98,48 +98,59 @@ function MDV(selection){
 	document.getElementById(id).innerHTML = text;
 	}
 
+//set a global variable with all the entities to be recalled if needed//
 var all_entities = document.getElementsByClassName("entity")
 
 //function openPopUp//
 function openPopUp(el){
 	let label = el.innerText;
-	for (i = 0; i < all_entities.length; i++) {
-		if (all_entities[i].getAttribute("data-label") == label) {
-			all_entities[i].classList.add("highlighted");
-			if (all_entities[i].hasAttribute("data-active")) {
-				//class_name = all_entities[i].getAttribute("class");
-				document.getElementById("PopUpHeader").innerHTML = label;
-				if (all_entities[i].hasAttribute("data-wikidata-id")) {
-					wikidataID = all_entities[i].getAttribute("data-wikidata-id");
-					document.getElementById("PopUpWikidata").innerHTML = "url es. https://www.wikidata.org/wiki/" + wikidataID;
-					document.getElementById("PopUpWikidata").href = "https://www.wikidata.org/wiki/" + wikidataID;
-					}
-				else {
-					document.getElementById("WikidataIntro").style.display="none"
-					}
-				document.getElementById("MOD_01").style.display="block";
-				}
-			}
+	var clicked_entity = ((Array.from(all_entities)).filter(entity => entity.getAttribute("data-label") == label));
+	clicked_entity.forEach(entity => entity.classList.add("highlighted"));
+	(clicked_entity.at(0)).classList.add("highlighted_more");
+	(clicked_entity.at(0)).scrollIntoView();
+	item_with_data = clicked_entity.filter(entity => entity.hasAttribute("data-active"));
+	document.getElementById("PopUpHeader").innerHTML = label;
+	if (item_with_data[0].hasAttribute("data-wikidata-id")) {
+		wikidataID = item_with_data[0].getAttribute("data-wikidata-id");
+		document.getElementById("PopUpWikidata").innerHTML = "url es. https://www.wikidata.org/wiki/" + wikidataID;
+		document.getElementById("PopUpWikidata").href = "https://www.wikidata.org/wiki/" + wikidataID;
 		}
+	else {
+		document.getElementById("WikidataIntro").style.display="none"
+		}
+	document.getElementById("MOD_01").style.display="block";
 	}
+	//cambiata la ricorsività per filtri: clicked_entity.classList.add("highlighted");
+	//for (i = 0; i < all_entities.length; i++) {
+	//	if (all_entities[i].getAttribute("data-label") == label) {
+	//		all_entities[i].classList.add("highlighted");
+	//		if (all_entities[i].hasAttribute("data-active")) {	
+	//wikidataID = all_entities[i].getAttribute("data-wikidata-id");
+	//class_name = all_entities[i].getAttribute("class");
 
 //function to slides entities occurencies//
 slide = 0
-function slide_clicked_entity(go) {
+function slide_clicked_entity(go){
 	let label = document.getElementById("PopUpHeader").innerText;
-	var clicked_entity = (Array.from(all_entities)).filter(entity => entity.getAttribute("data-label") == label);
-	//fino a lunghezza array while slide < clicked_entity.length - else slide = 0
-	if (go == 1) {
-		slide = slide+1;
+	var clicked_entity = ((Array.from(all_entities)).filter(entity => entity.getAttribute("data-label") == label));
+	if (clicked_entity.length <= 1) {
+		slide = 0
 		}
-	if (go == -1) {
-		slide = slide-1;
+	else {
+		if (go == 1) {
+			slide = slide+1;
+			(clicked_entity.at(slide-1)).classList.remove("highlighted_more");
+			};
+		if (go == -1) {
+			slide = slide-1;
+			(clicked_entity.at(slide+1)).classList.remove("highlighted_more");
+			};
+		if (Math.abs(slide) == clicked_entity.length)  {
+			slide = 0
+			}
 		}
-	var entity_occurency = clicked_entity.at(slide);
-	entity_occurency.classList.add("highlighted_current_occurency");
-	entity_occurency.scrollIntoView();
-	var entity_occurency_prev = clicked_entity.at(slide-1);
-	entity_occurency_prev.classList.remove("highlighted_current_occurency");
+	(clicked_entity.at(slide)).classList.add("highlighted_more");
+	(clicked_entity.at(slide)).scrollIntoView();
 	}
 
 //function to high_href() [working off-line with $(document).ready(function(){ not online]//

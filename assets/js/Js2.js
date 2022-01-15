fbcd0c50f46982a353fdad94d70b6801164e9b62
AlisiaZarbo <56439283+AlisiaZarbo@users.noreply.github.com>
2022-01-15 17:46:40 +0100

    Update Js2.js

--- assets/js/Js2.js
+++ assets/js/Js2.js
@@ -188,11 +188,6 @@ function slide_clicked_entity(go){
 	document.getElementById("counter_occurrencies").innerText = entity_counter;
 	}
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 //function closePopUp()//
 function closePopUp(){
@@ -214,9 +209,7 @@ $(".bib_note").click(function(){
 		});
 	})
 
-$( function() {
-    $( "MOD_01" ).draggable();
-  } );//function drag//
+//function drag//
 //function high_href(){//
 //$(window).load(function(){//
 //window.onload = function(){//
@@ -242,45 +235,4 @@ function myRes() {
 }
 };
 
-dragElement(document.getElementById("MOD_01"));
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}

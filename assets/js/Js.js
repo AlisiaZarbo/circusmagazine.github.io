78d956a349e7a8044d45ce2970cb006fdb19bd38
AlisiaZarbo <56439283+AlisiaZarbo@users.noreply.github.com>
2022-01-19 23:45:36 +0100

    Update Js.js

--- assets/js/Js.js
+++ assets/js/Js.js
@@ -210,7 +210,7 @@ function closePopUp(){
 	}
 
 //TOGLIERE.function to high_href() [working off-line with $(document).ready(function(){ not online]//
-$(".bib_note").click(function(){
+/*$(".bib_note").click(function(){
 		var target = $(this).attr("href");
 		var source_id = $(this).attr("id");
 		var source = document.getElementById(source_id);
@@ -219,7 +219,7 @@ $(".bib_note").click(function(){
 			$(target).removeClass('highlighted');
 			source.scrollIntoView();
 		});
-	})
+	})*/
 
 //function drag//
 //function high_href(){//

12f78404c564dc8e19cc76ecb70a043f505b534b
enri-ca <72966913+enri-ca@users.noreply.github.com>
2022-01-19 19:18:25 +0100

    Update Js.js

--- assets/js/Js.js
+++ assets/js/Js.js
@@ -103,7 +103,7 @@ var all_entities = document.getElementsByClassName("entity")
 //function openPopUp//
 function openPopUp(el){
 	(Array.from(all_entities)).forEach(entity => entity.classList.remove("highlighted", "highlighted_more"));
-	var clicked_entity = ()
+	var clicked_entity = [];
 	let label = el.innerText;
 	clicked_entity = ((Array.from(all_entities)).filter(entity => entity.getAttribute("data-label") == label));
 	clicked_entity.forEach(entity => entity.classList.add("highlighted"));
